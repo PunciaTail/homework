9b3d5b8b9c7c67d499f4b15f8fbd53f2b6b7840d
sangmin <leejeayu05@gmail.com>
2023-03-28 16:18:37 +0900

    create monster
    start in python file named game

---
+++ create.py
@@ -0,0 +1,65 @@
+import random
+import time
+
+
+class Character:
+    def __init__(self, name, hp, power, mp):
+        self.name = name
+        self.max_hp = hp
+        self.hp = hp
+        self.max_mp = mp
+        self.mp = mp
+        self.power = power
+
+    def attack(self, other):
+        damage = random.randint(self.power - 2, self.power + 2)
+        other.hp = max(other.hp - damage, 0)
+        print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+        if other.hp == 0:
+            print(f"{other.name}이(가) 쓰러졌습니다.")
+
+    def magic(self, other):
+        if self.mp > 0:
+            mg_damage = random.randint(self.mp - 4, self.mp + 7)
+            other.hp = max(other.hp - mg_damage, 0)
+            self.mp = max(self.mp - mg_damage, 0)
+            print(f"{self.name}의 마법 공격! {other.name}에게 {mg_damage}의 데미지를 입혔습니다.")
+            if self.mp < 0:
+                self.mp == 0
+        elif self.mp <= 0:
+            self.mp == 0
+
+        if other.hp == 0:
+            print(f"{other.name}이(가) 쓰러졌습니다.")
+
+    def show_status(self):
+        print(f"{self.name}의 상태: HP {self.hp}/{self.max_hp}")
+        if self.mp >= 0:
+            print(f"{self.name}의 상태: MP {self.mp}/{self.max_mp}")
+
+
+class SubMonster:
+    def __init__(self, name, list):
+        self.name = name
+        self.list = list
+
+
+# 이름 설정
+time.sleep(0.5)
+print("이름을 입력하세요")
+name = input()
+time.sleep(0.5)
+print(f'플레이어 생성: {name}')
+
+monster = ['ork', 'fairy', 'sphinx', 'magician']
+
+# 몬스터 랜덤 생성
+m_ran = random.choice(monster)
+hp_ran = random.randrange(25, 40)
+pw_ran = random.randrange(10, 35)
+
+# 플레이어 생성
+# 랜덤으로 몬스터 종류 변경
+if name != '':
+    player = Character(name, 30, 15, 20)
+    monster = Character(m_ran, hp_ran, pw_ran, -1)
---
+++ game.py
@@ -0,0 +1,66 @@
+from sphinx import *
+from create import *
+# from magician import *
+
+
+def call_generic():
+    input('전투를 시작합니다. 아무키나 누르세요.')
+
+    while player.hp > 0 and monster.hp > 0:
+
+        select = input("1마법 2공격")
+
+        if select == '1':
+            player.magic(monster)
+            time.sleep(0.5)
+            player.show_status()
+            monster.show_status()
+            time.sleep(1)
+            if monster.hp == 0:
+                print(f'{player.name}님 축하합니다. 승리하셨어요.')
+                break
+            monster.attack(player)
+            time.sleep(0.5)
+            player.show_status()
+            monster.show_stastus()
+            if player.hp == 0:
+                print('You die')
+                break
+
+        elif select == '2':
+            player.attack(monster)
+            time.sleep(0.5)
+            player.show_status()
+            monster.show_status()
+            time.sleep(1)
+            if monster.hp == 0:
+                print(f'{player.name}님 축하합니다. 승리하셨어요.')
+                break
+            monster.attack(player)
+            time.sleep(0.5)
+            player.show_status()
+            monster.show_status()
+            if player.hp == 0:
+                print('You die')
+                break
+
+        else:
+            print("알맞은 값을 입력하세요")
+
+        if monster.hp == 0:
+            print(f'{player.name}님 축하합니다. 승리하셨어요.')
+            break
+
+        elif player.hp == 0:
+            print('You die')
+            break
+
+    return
+
+
+if monster.name == 'sphinx':
+    call_sphinx()
+# elif monster.name == 'magician':
+#     call_magician()
+else:
+    call_generic()
